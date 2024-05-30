645860ed529ef03105fff085996ecb2917289fce
Elijah Wines <wines.elijah@gmail.com>
2024-05-30 19:06:23 -0400

    added react cookies and toastify and created login and signup page

--- dashboard/src/contexts/ContextProvider.js
+++ dashboard/src/contexts/ContextProvider.js
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useState } from 'react'
 import axios from 'axios'
 
-const BASE_URL = 'https://ecommerce-dashboard-rrsc.onrender.com/api/v1'
+const BASE_URL = 'http://localhost:3000/api/v1'
 
 const StateContext = createContext()
 
--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css'
 import App from './App';
 import { ContextProvider } from './contexts/ContextProvider'
+import 'react-toastify/dist/ReactToastify.css'
 
 ReactDOM.render(
     <ContextProvider>

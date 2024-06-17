93de5de1e9fbc6f11d9ab84e8ff9b13af44c9029
Elijah Wines <wines.elijah@gmail.com>
2024-06-17 18:25:21 -0400

    styled login and signup pages

--- dashboard/src/pages/Login.jsx
+++ dashboard/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
-import Header from '../components/Header'
+import { SiShopware } from 'react-icons/si'
 import { useStateContext } from '../contexts/ContextProvider'
 
 const Login = () => {
@@ -43,42 +43,52 @@ const Login = () => {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <Header category="" title="Login" />
+    <div className="flex justify-center items-center bg-slate-900 w-screen h-screen">
+      <div className="text-white p-10 bg-slate-800 rounded-2xl">
+        <form onSubmit={handleSubmit}>
           <div>
-            <input
-              type="text"
-              value={email}
-              name="email"
-              placeholder="Email"
-              onChange={handleInput('email')}
-            />
-          </div>
-          <div>
-            <input
-              type="password"
-              value={password}
-              name="password"
-              placeholder="Password"
-              onChange={handleInput('password')}
-            />
-          </div>
+            <p className="text-3xl flex gap-3 font-extrabold mb-5">
+              <SiShopware /> <span>Ecommerce Dashboard</span>
+            </p>
+            <p className="text-xl font-extrabold tracking-right mb-5">
+              Sign in to your account
+            </p>
+            <div>
+              <label>Email</label>
+              <input
+                className="w-full p-2 bg-slate-700 rounded-lg my-2"
+                type="text"
+                value={email}
+                name="email"
+                placeholder="name@company.com"
+                onChange={handleInput('email')}
+              />
+            </div>
+            <div>
+              <label>Password</label>
+              <input
+                className="w-full p-2 bg-slate-700 rounded-lg my-2"
+                type="password"
+                value={password}
+                name="password"
+                placeholder="Password"
+                onChange={handleInput('password')}
+              />
+            </div>
 
-          <div className='p-10'>
+            <div className='py-5'>
               <button
-                className='text-xl p-3 w-1/3 rounded-xl hover:drop-shadow-xl'
-                style={{ backgroundColor: currentColor }}
+                className='text-xl p-3 w-full rounded-xl hover:drop-shadow-xl bg-blue-500 hover:bg-blue-700'
               >
                 Login
               </button>
-              <span>
-                Don't have an account? <Link to={"/signup"}>Signup</Link>
-              </span>
+            </div>
+            <div>
+              <div>Don't have an account? <span className="text-blue-500 hover:underline"><Link to={"/signup"}>Signup</Link></span></div>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
       <ToastContainer />
     </div>
   )
--- dashboard/src/pages/Signup.jsx
+++ dashboard/src/pages/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
-import Header from '../components/Header'
+import { SiShopware } from 'react-icons/si'
 import { useStateContext } from '../contexts/ContextProvider'
 
 const Signup = () => {
@@ -45,52 +45,64 @@ const Signup = () => {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <Header category="" title="Create An Account" />
+    <div className="flex justify-center items-center bg-slate-900 w-screen h-screen">
+      <div className="text-white p-10 bg-slate-800 rounded-2xl">
+        <form onSubmit={handleSubmit}>
           <div>
-            <input
-              type="text"
-              value={email}
-              name="email"
-              placeholder="Email"
-              onChange={handleInput('email')}
-            />
-          </div>
-          <div>
-          <input
-              type="text"
-              value={username}
-              name="username"
-              placeholder="Username"
-              onChange={handleInput('username')}
-            />
-          </div>
-          <div>
-          <input
-              type="password"
-              value={password}
-              name="password"
-              placeholder="Password"
-              onChange={handleInput('password')}
-            />
-          </div>
+            <p className="text-3xl flex gap-3 font-extrabold mb-5">
+              <SiShopware /> <span>Ecommerce Dashboard</span>
+            </p>
+            <p className="text-xl font-extrabold tracking-right mb-5">
+              Sign in to your account
+            </p>
+            <div>
+              <label>Email</label>
+              <input
+                className="w-full p-2 bg-slate-700 rounded-lg my-2"
+                type="text"
+                value={email}
+                name="email"
+                placeholder="name@company.com"
+                onChange={handleInput('email')}
+              />
+            </div>
+            <div>
+              <label>Username</label>
+              <input
+                className="w-full p-2 bg-slate-700 rounded-lg my-2"
+                type="text"
+                value={username}
+                name="username"
+                placeholder="Username"
+                onChange={handleInput('username')}
+              />
+            </div>
+            <div>
+              <label>Password</label>
+              <input
+                className="w-full p-2 bg-slate-700 rounded-lg my-2"
+                type="password"
+                value={password}
+                name="password"
+                placeholder="Password"
+                onChange={handleInput('password')}
+              />
+            </div>
 
-          <div className='p-10'>
+            <div className='py-5'>
               <button
-                className='text-xl p-3 w-1/3 rounded-xl hover:drop-shadow-xl'
-                style={{ backgroundColor: currentColor }}
+                className='text-xl p-3 w-full rounded-xl hover:drop-shadow-xl bg-blue-500 hover:bg-blue-700'
               >
                 Signup
               </button>
-              <span>
-                Already have an account? <Link to={"/login"}>Login</Link>
-              </span>
+            </div>
+            <div>
+              <div>Already have an account? <span className="text-blue-500 hover:underline"><Link to={"/login"}>Login</Link></span></div>
+            </div>
           </div>
-        </div>
-      </form>
-      <ToastContainer />
+        </form>
+        <ToastContainer />
+      </div>
     </div>
   )
 }

a2461e7a4883eb5568d78652001174dc7d53754c
Elijah Wines <wines.elijah@gmail.com>
2024-07-01 17:49:21 -0400

    updated BASE_URL and CORS

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
     cors({
-        origin: "http://localhost:3001",
+        origin: ["http://localhost:3001", "https://ecommerce-dashboard-frontend-9rxr.onrender.com"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })

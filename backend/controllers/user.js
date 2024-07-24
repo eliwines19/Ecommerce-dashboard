d182b463ab3e9ffca5b5f091255ef01e061d77cf
Elijah Wines <wines.elijah@gmail.com>
2024-07-24 17:59:28 -0400

    added updateColorTheme User Controller method

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,10 +1,13 @@
 const UserSchema = require("../models/UserModel");
 
-exports.getColorTheme = async (req, res) => {
+exports.updateColorTheme = async (req, res) => {
     const { id } = req.params;
+    const { colorTheme } = req.body;
     try {
-        const user = await UserSchema.findById(id)
-        return res.status(200).json({ colorTheme: user.colorTheme })
+        const user = await UserSchema.findById(id);
+        user.colorTheme = colorTheme;
+        await user.save();
+        return res.status(200).json({ message: "Color Theme Successfully Updated" })
     } catch (error) {
         return res.status(500).json({ Error: error })
     }
